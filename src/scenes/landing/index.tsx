71642c0f5a1211d5f0d9a56e5355d958c8094ff3
Francis-Romeo-Remolacio <145172462+Francis-Romeo-Remolacio@users.noreply.github.com>
2024-10-06 02:59:12 +0800

    Update landing page

--- src/scenes/landing/index.tsx
+++ src/scenes/landing/index.tsx
@@ -10,11 +10,16 @@ import {
   ListItemText,
   Box,
   Grid2 as Grid,
+  useTheme,
 } from "@mui/material";
 import { Helmet } from "react-helmet-async";
 import DesignGallery from "./../../components/DesignGallery.jsx";
+import { Tokens } from "../../Theme.ts";
 
 const Landing = () => {
+  const theme = useTheme();
+  const colors = Tokens(theme.palette.mode);
+
   return (
     <>
       <Box
@@ -25,11 +30,14 @@ const Landing = () => {
           top: 0,
           zIndex: -1,
           opacity: 0.5,
-          margin: -2,
+          m: -2,
+          backgroundColor: colors.primary[100],
           background: `url(banner.png) center/cover no-repeat`,
+          maskImage:
+            "linear-gradient(to bottom, rgba(0,0,0,1) 0%,  rgba(0,0,0,1) 90%, rgba(0,0,0,0) 100%)",
         }}
       />
-      <Stack justifyContent="center" spacing={4} mt="0">
+      <Stack justifyContent="center" spacing={4} mt="0" sx={{ zIndex: 25 }}>
         <Helmet>
           <title>The Pink Butter Cake Studio</title>
         </Helmet>
@@ -55,23 +63,41 @@ const Landing = () => {
         </Stack>
 
         <Grid container spacing={2}>
-          <Grid size={{ xs: 12, md: 6, lg: 4 }}>
+          <Grid size={{ xs: 12, md: 6, lg: 3 }}>
             {/* Section 2: About */}
             <Paper sx={{ p: 2, height: "100%" }}>
               <Typography variant="h4" gutterBottom>
-                About The Pink Butter Cake Studio?
+                About The Pink Butter Cake Studio
               </Typography>
-              <Typography variant="body1" paragraph>
-                Established in 2017, The Pink Butter Cake Studio has been
-                crafting stunning and delicious cakes for all occasions.
+              <Typography variant="body1">
+                The Pink Butter Cake Studio, established in 2017, has quickly
+                become a beloved destination for cake enthusiasts in Quezon
+                City, Philippines. Specializing in custom orders, the studio
+                prides itself on creating exquisite cakes tailored to suit any
+                theme or occasion, from birthdays and weddings to corporate
+                events. With a commitment to quality and creativity, each cake
+                is meticulously crafted to ensure not only a visually stunning
+                presentation but also exceptional flavor. The team at The Pink
+                Butter Cake Studio works closely with clients to understand
+                their vision, allowing for personalized touches that make every
+                cake a unique centerpiece for any celebration.
               </Typography>
-              <Typography variant="body1" paragraph>
-                Located at 5 Masbate St. Brgy. Nayong Kanluran, Quezon City,
-                Philippines.
+              <Typography variant="body1">
+                In addition to custom creations, The Pink Butter Cake Studio
+                offers a selection of premade designs that reflect the latest
+                trends and seasonal themes. These beautifully designed cakes are
+                perfect for those who want the same level of artistry and taste
+                without the wait for a custom order. With a convenient location
+                on Masbate Street in Brgy. Nayong Kanluran, the studio has
+                established itself as a go-to spot for cake lovers looking for
+                high-quality, handcrafted desserts. The Pink Butter Cake Studio
+                continues to delight customers with its innovative designs and
+                delicious flavors, ensuring that every occasion is celebrated
+                with a slice of sweetness.
               </Typography>
             </Paper>
           </Grid>
-          <Grid size={{ xs: 12, md: 6, lg: 4 }}>
+          <Grid size={{ xs: 12, md: 6, lg: 6 }}>
             {/* Section 3: Premade Designs */}
             <Paper sx={{ p: 2, height: "100%" }}>
               <Typography variant="h4" gutterBottom>
@@ -96,7 +122,7 @@ const Landing = () => {
         </Grid>*/}
 
           {/* Section 5: Testimonials */}
-          <Grid size={{ xs: 12, md: 6, lg: 4 }}>
+          <Grid size={{ xs: 12, md: 6, lg: 3 }}>
             <Paper sx={{ p: 2, height: "100%" }}>
               <Typography variant="h4" gutterBottom>
                 What Our Customers Say
